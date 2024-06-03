28bb0aba4e34b94b525b207a9ebf2f73f81fa0be
Nachin <ruben.godoy@ngg.cl>
2024-06-03 13:17:38 -0400

    Se termina prueba tecnica

--- 03-prueba-tecnica/src/App.jsx
+++ 03-prueba-tecnica/src/App.jsx
@@ -1,20 +1,49 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "https://catfact.ninja/fact";
+const API_URL_FACT = "https://catfact.ninja/fact";
+const API_URL_CLEAN = "https://cataas.com/cat/";
 
 export function App() {
-  const [fact, setFact] = useState("esto es un hecho");
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
   useEffect(() => {
-    fetch(API_URL)
+    fetch(API_URL_FACT)
       .then((res) => res.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+       
+      });
   }, []);
+
+
+useEffect(() => {
+
+  if (!fact) return;
+
+  const firstWord = fact.split(" ", 3).join(" ");
+  const API_URL_IMG = `${API_URL_CLEAN}/says/${firstWord}?fontColor=white&json=true`;
+  fetch(API_URL_IMG)
+    .then((res) => res.json())
+    .then((response) => {
+      const { _id } = response;
+
+      setImageUrl(
+        `${API_URL_CLEAN}${_id}/says/${firstWord}?fontSize=50&fontColor=white`
+      );
+    });
+
+} , [fact])
+
+
 
   return (
     <main>
       <h1>App de Gatos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt="Imagen obtenida aleatoriamente" />}
     </main>
   );
 }
