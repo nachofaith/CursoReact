e94c7d28f0fb8487810e14427b04723fee017ddb
Nachin <ruben.godoy@ngg.cl>
2024-06-02 23:58:27 -0400

    se avanza con la api de gatitos esta vez sync con github

--- 03-prueba-tecnica/src/App.jsx
+++ 03-prueba-tecnica/src/App.jsx
@@ -2,25 +2,19 @@ import { useEffect, useState } from "react";
 
 const API_URL = "https://catfact.ninja/fact";
 
+export function App() {
+  const [fact, setFact] = useState("esto es un hecho");
 
-export function App(){
-
-const [fact, setFact] = useState('esto es un hecho')
-
-useEffect(() => {
+  useEffect(() => {
     fetch(API_URL)
-    .then(res => res.json())
-},[])
+      .then((res) => res.json())
+      .then((data) => setFact(data.fact));
+  }, []);
 
-
-    return(
-        <main>
-     <h1>App de Gatos</h1>
-     <p>{fact}</p>
-
-
-        </main>
-   
-
-    )
+  return (
+    <main>
+      <h1>App de Gatos</h1>
+      <p>{fact}</p>
+    </main>
+  );
 }
