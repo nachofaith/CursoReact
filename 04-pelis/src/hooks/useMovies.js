8d5dc01a35cab6916609676697721264101cf923
Nachin <ruben.godoy@ngg.cl>
2024-06-18 16:25:12 -0400

    Se avanza en validaciones

---
+++ 04-pelis/src/hooks/useMovies.js
@@ -0,0 +1,16 @@
+import responseMovies from "../mocks/results.json";
+
+export function useMovies () {
+
+    const movies = responseMovies.Search;
+    const mappedMovies = movies?.map(movie => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster
+    }))
+
+    return { movies: mappedMovies}
+
+  
+}
